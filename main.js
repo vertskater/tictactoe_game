42844bdcd6a99657ab7b86595ef4272830b1ffb1
vertskater <vertskater@gmx.at>
2021-01-23 13:43:09 +0100

    implementet logic for populating the screen and basic game logic

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // Gamestart and Player init
-const game = (() => {
+const gameInit = (() => {
     //Init Arrays
     let playerNames = [];
     let players = [];
@@ -12,17 +12,17 @@ const game = (() => {
     let gameStartRender = document.querySelector('#player-render');
     let gameBoardHtml = document.querySelector('#gameboard');
     //Create Events
-    btnStartGame.addEventListener('click', startGame)
+    btnStartGame.addEventListener('click', _startGame)
     //logic to start the game
-    function inputHasValue(){
+    function _inputHasValue() {
         let hasVal = false;
-        for(let player of playerNamesInput){
-           return player.value !== '' ? hasVal = true : hasVal = false;
+        for (let player of playerNamesInput) {
+            return player.value !== '' ? hasVal = true : hasVal = false;
         }
     }
-    function startGame() {
+    function _startGame() {
         let playersCount = playerNamesInput.length;
-        if (inputHasValue()) {
+        if (_inputHasValue()) {
             for (let player of playerNamesInput) {
                 playerNames.push(player.value)
             }
@@ -35,17 +35,58 @@ const game = (() => {
             gameStartRender.style.display = 'none';
             gameBoardHtml.style.display = 'grid';
         }
-        createPlayers();
+        _createPlayers();
     }
-    //Factory Function to create Players
+    //Factory Function for Players
     const Player = (name, sign) => {
-        return { name, sign }
+        let turn = false;
+        return { name, sign, turn }
     }
-    function createPlayers() {
+    // Function to create Players
+    function _createPlayers() {
         for (let i in playerNames) {
             players[i] = Player(playerNames[i], playerSign[i]);
         }
     }
-
-    return {players} 
+    return { players, gameBoardHtml }
 })()
+
+const game = (() => {
+    let players = gameInit.players;
+    let gameBoard = {
+        gameBoardHtml: gameInit.gameBoardHtml,
+        board: [],
+        gameController: function (players) {
+            this.gameBoardHtml.addEventListener('click', (e) => {
+                if (e.target.matches('.box')) {
+                    let key = e.target;
+                    let index = key.dataset.index;
+                    let playerIndex = _playerTurn();
+                    if (playerIndex % 2 === 0) {
+                        this.board[index] = players[0].sign;
+                        _render(key, 0);
+                    }else{
+                        this.board[index] = players[1].sign;
+                        _render(key, 1);
+                    }
+                    //console.log(players[0].name, players[1].name)
+                }
+            })
+        }
+    }
+    gameBoard.board.length = 9;
+    function _playerTurn() {
+        let index = 0;
+        for (let item of gameBoard.board) {
+            item === undefined ? index += 0 : index++
+        }
+        return index
+    }
+    function _render(html, index){
+        html.textContent = players[index].sign;
+    }
+    gameBoard.gameController(players);
+    
+    return gameBoard;
+})(gameInit.players, gameInit.gameBoardHtml)
+
